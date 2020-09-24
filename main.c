9878ba17be666023f8b796d9b6b65d5f00f9244e
scarfaced00 <jazmyne@naver.com>
2020-09-24 12:59:40 +0900

    ex4

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-/*실습3*/
+/*실습4*/
 int main()
 {
-	int sec;
+	int year;
 
-	printf("input the second :");
-	scanf("%d", &sec);
-	printf("the time is %d : %d\n", sec / 60, sec % 60);
+	printf("input the year :");
+	scanf("%d", &year);
 
+	//400으로 나누어 떨어지면 윤년임
+	if (year == 400)
+		printf("is the year %d the leap year? : %i", year, year % 400 == 0);
+	else //아니면 4로나눠지는데 100으로 나누어지지않으면
+	{
+		printf("is the year %d the leap year? : %i", year, (year % 4 == 0) && (year % 100 != 0));
+	}
 	return 0;
+
 }
